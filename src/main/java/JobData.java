35f0a5ba7c991fe3ac59953c9155bb9cf083d693
alenag719 <alenag719@gmail.com>
2023-01-19 16:48:01 -0800

    completed

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -78,6 +78,8 @@ public class JobData {
         for (HashMap<String, String> row : allJobs) {
 
             String aValue = row.get(column);
+            aValue = aValue.toLowerCase();
+            value = value.toLowerCase();
 
             if (aValue.contains(value)) {
                 jobs.add(row);
@@ -99,7 +101,19 @@ public class JobData {
         loadData();
 
         // TODO - implement this method
-        return null;
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+        //return null;
+        for (HashMap<String, String> row : allJobs) {
+
+            String jobSearch = row.entrySet().toString().toLowerCase();
+            String lcValue = value.toLowerCase();
+
+            if (jobSearch.contains(lcValue)) {
+                jobs.add(row);
+            }
+        }
+
+        return jobs;
     }
 
     /**
